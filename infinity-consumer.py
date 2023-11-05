8f03623916a5bbf124b9db6229e2ae069f3fc763
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-11-05 11:50:38 +0000

    Add ability to change view

--- infinity-consumer.py
+++ infinity-consumer.py
@@ -6,6 +6,10 @@ from dataclasses import dataclass
 from math import log10
 from concurrent.futures import ProcessPoolExecutor
 import time
+import aioconsole
+import logging
+
+logging.getLogger('hypercorn.access').disabled = True
 
 @dataclass
 class Message():
@@ -27,11 +31,15 @@ consumer.subscribe(["symbols"])#, on_assign=reset_offsets)
 
 app = Quart(__name__)
 
+found_data = False
+
 messages = {}
 
 latest_times = {}
 
 average_volumes = {}
+
+current_symbol = "BINANCE:BTCUSDT"
 
 @app.route("/")
 def hello_world():
@@ -40,19 +48,11 @@ def hello_world():
 @app.route("/api/bubbles")
 def bubbles():
     try:
-        print("A")
-        current_symbol = "BINANCE:BTCUSDT"
-        
         if not current_symbol in average_volumes:
             return "-1"
-        # return "Bubbles"
-        print("A")
-        print(average_volumes)
         result = str(max(0.75, min(5 - 1.55 * log10(average_volumes[current_symbol]), 5)))
-        print("B")
-        print("result", result)
         title = current_symbol.split(":")[-1]
-        hue = (SYMBOLS.index(current_symbol) * 16) % 256
+        hue = (SYMBOLS.index(current_symbol) * 32) % 256
     except Exception as e:
         print(e)
     return ",".join([result, title, str(hue)])
@@ -62,14 +62,16 @@ async def run_server():
     app.run(host="0.0.0.0", port=8080)
 
 async def consume():
-    global messages, average_volumes
+    global messages, average_volumes, found_data
     while True:
         try:
             message_data = consumer.poll(timeout=1)
             if message_data is None:
-                print(consumer)
                 await asyncio.sleep(0.1)
                 continue
+            if not found_data:
+                found_data = True
+                print("Found data!")
             json_data = json.loads(message_data.value().decode())
             symbol = json_data["s"]
             if not symbol in messages:
@@ -82,8 +84,8 @@ async def consume():
             )
             messages[symbol].append(message)
             latest_times[symbol] = max(latest_times[symbol] if symbol in latest_times else 0, message.unix_time)
-            if "BINANCE:ETHUSDT" in messages:
-                print(len(messages["BINANCE:ETHUSDT"]))
+            # if "BINANCE:ETHUSDT" in messages:
+            #     print(len(messages["BINANCE:ETHUSDT"]))
             for symbol in messages:
                 messages[symbol] = list(filter(lambda m: m.unix_time >= latest_times[symbol] - 10 * 1000, messages[symbol]))
                 average_volumes[symbol] = sum([m.volume for m in messages[symbol]]) / (len(messages[symbol]) + 0.1)
@@ -93,9 +95,18 @@ async def consume():
             print("Consumer is closed.")
             break
 
+async def check_for_input():
+    global current_symbol
+    while True:
+        await aioconsole.ainput('')
+        current_symbol = SYMBOLS[(SYMBOLS.index(current_symbol) + 1) % len(SYMBOLS)]
+        print(f"Now viewing {current_symbol}")
+
+
 @app.before_serving
 async def startup():
     loop = asyncio.get_event_loop()
+    loop.create_task(check_for_input())
     loop.create_task(consume())
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=8080,)
